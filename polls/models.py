90c80572c76cee466221e5b973a0fa7d206173bc
Hui Chen <33416819+edgarhuichen@users.noreply.github.com>
2018-04-23 13:12:35 +0200

    Set up admin page
    Get familiar with shell API interface and set up admin page.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 '''In our simple poll app, we’ll create two models: Question and Choice. 
 A Question has a question and a publication date. A Choice has two fields: 
 the text of the choice and a vote tally. Each Choice is associated with a Question.'''
@@ -8,8 +10,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
